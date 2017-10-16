43737933b64b25fae95d0bb73203898ae39964f9
ITIS\LORENZO.MALFERRARI <malfe.lore@gmail.com>
2017-10-16 12:27:43 +0200

    Add project

---
+++ Factory/Factory/BMW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    class BMW : ICar
+    {
+        private string targa;
+        public string Model => "BMW Serie 3 Touring";
+        public BMW(string targa)
+        {
+            this.targa = targa;
+        }
+    }
+}
---
+++ Factory/Factory/Renter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    class Renter
+    {
+        public static ICar Rent(double budget)
+        {
+            if (budget <= 80)
+            {
+                return new Toyota();
+            }
+            else if (budget > 80 && budget < 150)
+            {
+                return new Catorcio();
+            }
+            else
+            {
+                return new Mercedes();
+            }
+        }
+    }
+}
